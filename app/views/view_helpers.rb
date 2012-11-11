6bcdcb2559acd11fc4dc907c44d7df84b120047a
Ale Muñoz <bomberstudios@gmail.com>
2012-11-11 16:06:16 +0100

    Added analytics helper

--- app/views/view_helpers.rb
+++ app/views/view_helpers.rb
@@ -28,4 +28,19 @@ module ViewHelpers
     return ['philosophy','workflow','documentation','tips','updating']
   end
 
+  def google_analytics
+    if ENV['RACK_ENV'] == 'production' && ENV['GA'] != nil
+      javascript_tag "
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', '#{ENV['GA']}']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();"
+    end
+  end
+
 end
